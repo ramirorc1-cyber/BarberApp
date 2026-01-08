33b9fe996f6047977babc3ff9bd8b42615230342
ramirorc1-cyber <ramiro_rc1@tesch.edu.mx>
2026-01-08 16:37:51 -0600

    avances

--- app/src/main/java/com/example/barberapp/CitaDao.kt
+++ app/src/main/java/com/example/barberapp/CitaDao.kt
@@ -2,10 +2,14 @@ package com.example.barberapp
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface CitaDao {
 
     @Insert
     suspend fun insertarCita(cita: Cita)
+    @Query("SELECT * FROM citas ORDER BY fecha, hora")
+    suspend fun obtenerCitas(): List<Cita>
+
 }
